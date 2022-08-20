c1f7513bd070829e5715df9219c7cc1e4e652806
Gian Dieo Javes <gjaves.tnegocios@gmail.com>
2022-08-20 12:41:38 -0500

    fix wit and extensions.conf

--- Utilidades/AGI-SERVER/index.js
+++ Utilidades/AGI-SERVER/index.js
@@ -1,45 +1,62 @@
-const AGIServer = require('voxo-agi').AGIServer;
-const {Wit, log} = require('node-wit');
+const AGIServer = require("voxo-agi").AGIServer;
+const { Wit, log } = require("node-wit");
 
 const client = new Wit({
-    accessToken: 'A6C7R2FTXPCDUMVSTRDS3ELERPI2UIC2',
-    logger: new log.Logger(log.DEBUG) // optional
+  accessToken: "EGOFXRGUVGUC4RMS3MVWF775ZWDCD6FX",
+  logger: new log.Logger(log.DEBUG), // optional
 });
 
 async function testScript(channel) {
-    console.log('Script got call %s -> %s', channel.request.callerid, channel.request.extension);
+  console.log(
+    "Script got call %s -> %s",
+    channel.request.callerid,
+    channel.request.extension
+  );
 
-    let asr = await channel.request.arg_1;
-    console.log('asr: ',asr);
+  let asr = await channel.request.arg_1;
+  console.log("asr: ", asr);
 
-    await client.message(asr, {})
+  await client
+    .message(asr, {})
     .then(async (data) => {
-        
-        //console.log('data: ',data);
+      console.log("data: ", data.intents[0]);
 
-        //let result = data.intents.find(o => o.confidence > '0.89');
+      //validamos que no traiga vació el resultado
+      if (!data.intents[0]) {
+        let wit = await channel.setVariable("wit", "none");
+        return 200;
+      }
+      //let result = data.intents.find(o => o.confidence > '0.89');
 
-        const result = data.intents.reduce((prev, current) => (prev.confidence > current.confidence) ? prev : current)
+      const result = data.intents.reduce((prev, current) =>
+        prev.confidence > current.confidence ? prev : current
+      );
 
-        console.log(result);
+      console.log("Result: ", result);
 
-        if (result) {
-            let wit = await channel.setVariable('wit',result.name);
-            console.log(wit);
-            return 200;
-        }else{
-            let wit = await channel.setVariable('wit','error');
-            return 200;
-        }
-        
+      if (result) {
+        //si quieren poner mas código así:
+        // if (result.confidence > '0.89') {
+        //      let wit = await channel.setVariable('wit',result.name);
+        // } else {
+        //     let wit = await channel.setVariable('wit','none');
+        // }
 
+        let wit =
+          result.confidence > "0.89"
+            ? await channel.setVariable("wit", result.name)
+            : await channel.setVariable("wit", "none");
+        //let wit = await channel.setVariable('wit',result.name);
+        console.log("Wit: ", wit);
+        return 200;
+      } else {
+        let wit = await channel.setVariable("wit", "error");
+        return 200;
+      }
     })
     .catch(console.error);
-
-
-
 }
- 
+
 /* AGI Server */
-console.log('Iniciamos AGIServer...');
+console.log("Iniciamos AGIServer...");
 var server = new AGIServer(testScript, 4573);

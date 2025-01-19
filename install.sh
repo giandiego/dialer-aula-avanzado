292540208cb6e010cb94d5c56ed31cf44caaf3e8
giandiego <gjaves.tnegocios@gmail.com>
2025-01-19 11:49:40 -0500

    Refactor installation script to use dnf instead of yum and update Node.js setup to version 22.x. Add local dependencies 'api-base-mongo-jwt' and 'agi-server' in package.json files.

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
-yum update -y
-yum  -y install epel-release
+dnf update -y
+dnf -y install epel-release
 
 systemctl stop firewalld
-yum install -y certbot
+dnf install -y certbot
 systemctl start firewalld
 
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-sudo yum clean all && sudo yum makecache fast
-sudo yum install -y gcc-c++ make
-sudo yum install -y nodejs
+curl -sL https://rpm.nodesource.com/setup_22.x | sudo bash -
+sudo dnf clean all && sudo dnf makecache
+sudo dnf install -y gcc-c++ make
+sudo dnf install -y nodejs
 npm i -g pm2 --force
+
 cp env-example .env
